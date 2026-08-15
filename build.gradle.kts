b41371273045b1da96f075c8e762087330ea305f
Shiyas Moideen Kutty <120125895+Shiyaspmk@users.noreply.github.com>
2026-08-15 07:10:21 +0400

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file. Individual module build files apply plugins with the
+// versions declared here (see app/build.gradle.kts).
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+}
